d486d9192b0488326da9791745210227b74c6ae4
naveenmare <77376044+naveenmare@users.noreply.github.com>
2022-05-17 22:56:11 +0530

    Connected log in and sign up pages to json server

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,8 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
@@ -13,34 +11,26 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 const theme = createTheme();
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    //const data = new FormData(event.currentTarget);
+    // console.log({
+    //   email: data.get("email"),
+    //   password: data.get("password"),
+    // });
+    const res = await fetch(
+      "http://localhost:5000/users?email=" + email + "&password=" + password
+    );
+    const data = await res.json();
+    if (data[0]) {
+      alert("Correct email and password");
+    } else alert("Incorrect email or password");
   };
 
   return (
@@ -94,6 +84,7 @@ export default function Login() {
                 name="email"
                 autoComplete="email"
                 autoFocus
+                onChange={(e) => setEmail(e.target.value)}
               />
               <TextField
                 margin="normal"
@@ -104,6 +95,7 @@ export default function Login() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
               />
               <Button
                 type="submit"
@@ -125,7 +117,6 @@ export default function Login() {
                   </Link>
                 </Grid>
               </Grid>
-              {/* <Copyright sx={{ mt: 5 }} /> */}
             </Box>
           </Box>
         </Grid>
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -3,8 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
@@ -13,34 +11,50 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 const theme = createTheme();
 
+type UserData = {
+  name: string;
+  email: string;
+  password: string;
+};
+
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [email, setEmail] = React.useState<string>("");
+  const [name, setName] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  const [repeatPassword, setRepeatPassword] = React.useState<string>("");
+
+  const validateEmail = async (email: string): Promise<boolean> => {
+    const res = await fetch("http://localhost:5000/users?email=" + email);
+    const data = await res.json();
+    if (!data[0]) {
+      return true;
+    } else {
+      alert("Email already exists");
+      return false;
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    const userData: UserData = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    if (password === repeatPassword) {
+      const res = await fetch("http://localhost:5000/users", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+    } else {
+      alert("Password does not match");
+    }
   };
 
   return (
@@ -89,11 +103,12 @@ export default function Login() {
                 margin="normal"
                 required
                 fullWidth
-                id="email"
+                id="name"
                 label="Name"
-                name="email"
-                autoComplete="email"
+                name="name"
+                autoComplete="name"
                 autoFocus
+                onChange={(e) => setName(e.target.value)}
               />
               <TextField
                 margin="normal"
@@ -103,7 +118,9 @@ export default function Login() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
               />
+              <h1>{email}</h1>
               <TextField
                 margin="normal"
                 required
@@ -113,6 +130,7 @@ export default function Login() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
               />
               <TextField
                 margin="normal"
@@ -122,7 +140,8 @@ export default function Login() {
                 label="Repeat Password"
                 type="password"
                 id="repeatPassword"
-                autoComplete="current-password"
+                autoComplete="repeat-password"
+                onChange={(e) => setRepeatPassword(e.target.value)}
               />
               <Button
                 type="submit"
@@ -140,7 +159,6 @@ export default function Login() {
                   </Link>
                 </Grid>
               </Grid>
-              {/* <Copyright sx={{ mt: 5 }} /> */}
             </Box>
           </Box>
         </Grid>
